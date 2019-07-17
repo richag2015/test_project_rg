5aafdb896b5fc675de1ceececdc098ea29ebb3d7
richag2015 <46906463+richag2015@users.noreply.github.com>
2019-07-17 15:25:00 -0400

    Create plot.m

---
+++ plot.m
@@ -0,0 +1,13 @@
+%% Creating an anonymous identity function
+% An identity function returns its input arguments. This script 
+% demonstrates one way to create an anonymous identity function.
+%% Create the function
+I = @(X) X;
+%% Use the function
+% Pass the Unicode Black Star (U+2605; ?)
+Y = I('?');
+%% Display the result
+% This is only necessary in this example.
+disp(Y)
+%% Making a plot using the function
+plot(I(rand(10)));
